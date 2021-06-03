48095cb86f522c1ce952bb4205efb7dc5465ff6e
srikanthnani <srikanthnani1202@gmail.com>
2021-06-03 10:58:36 +0530

    On Board Screen

--- src/core/Strings.js
+++ src/core/Strings.js
@@ -2,6 +2,53 @@ const DEFAULT_STRINGS = {
     APPNAME: "HomeRental-ui",
 }
 
+const ONBOARD_STRINGS = {
+    ONBOARD_ARRAY: [
+        {
+            ID: 1,
+            IMG: "https://pixabay.com/get/gcafb61adb73b1c5dcc704ff9bb382a1ca1111d7b89a14ef398ab4cd94ee411db2e99de282db6e62fca7aed70a266364f_1280.jpg",
+            TITLE: "Find your\nGreat Home",
+            CONTENT: "Schedule visits in just a few clicks\nvisit in just a few clicks."
+        },
+        {
+            ID: 2,
+            IMG: "https://pixabay.com/get/g1200af6470e44102cedd82f34f95d9cfb3d515dceb2a25d157df889cc5b8a872f7a5c4b646426131bb741abd583d4ef7_1280.jpg",
+            TITLE: "Find your\nBest Home",
+            CONTENT: "You are not buying a Home,\nyou are buying a lifestyle."
+        },
+        {
+            ID: 3,
+            IMG: "https://pixabay.com/get/g62fcd1bcd1921cbc1f006be9ccc740013e9f240a3077fa09df41c8f38f336607328a8a046fa405f739942e7957d5bb19_1280.jpg",
+            TITLE: "Find your\nSweet Home",
+            CONTENT: "Schedule visits in just a few clicks\nvisit in just a few clicks."
+        }
+    ],
+    BUTTON_NEXT: "Next",
+    BUTTON_START: "Get Started",
+    BUTTON_STARTAGAIN: "Clear",
+}
+
+const HOME_STRINGS = {
+    LOCATION: "Location",
+    DEFAULT_LOCATION: "New York",
+    SEARCH: "Search address, city, location",
+    CATEGORYLIST: ['Popular', 'Recommended', 'Nearest'],
+}
+
+const DETAILS_STRINGS = {
+    TOTAL_PRICE: "Total Price",
+    BOOKNOW: "Book Now",
+    RATINGS: "ratings",
+    BEDS: "Beds",
+    BATHS: "Baths",
+    AREA: "m area",
+    VIRTUAL_TOUR: "Virtual tour",
+    CURRENCY: "$"
+}
+
 export {
     DEFAULT_STRINGS,
+    ONBOARD_STRINGS,
+    HOME_STRINGS,
+    DETAILS_STRINGS
 }
